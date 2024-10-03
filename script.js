0469dcfa5b6a83f6c07bbea90a89bab08692d660
alexandru233847 <alexutualexino33@gmail.com>
2024-10-03 18:48:43 +0200

    finish

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const emailList = ["lysanne_schuppe@gmail.com", "bernhard_torphy50@yahoo.com", "
 console.log(emailList);
 
 // Ask the user to enter their email
-const emailUser = prompt("Enter your email")
+const emailUser = prompt("Enter your email");
 
 // Print an appropriate message about the check result
 if
     (emailList.includes(emailUser)) {
-    console.log("Email authorized email")
+    console.log("Email authorized email");
 } else {
-    console.log("access denied")
+    console.log("access denied");
 }
 
 
+const scorePlayer = Math.floor(Math.random() * 6) + 1;
+console.log('you rolled: $ {scorePlayer}');
 
+const scoreComputer = Math.floor(Math.random() * 6) + 1;
+console.log('computer rolled: $ {scoreComputer}');
 
+// Determinate the winner
+if (scorePlayer > scoreComputer) {
+    console.log("You win");
+} else if (scorePlayer < scoreComputer) {
+    console.log ("computer win");
+} else {
+    console.log("it's a tie");
+}
 

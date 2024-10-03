8cba79c3c86635756badcc6392ffe2cfc399131f
alexandru233847 <alexutualexino33@gmail.com>
2024-10-03 16:32:27 +0200

    Enter your email

---
+++ script.js
@@ -0,0 +1,19 @@
+// Email list
+const emailList = ["lysanne_schuppe@gmail.com", "bernhard_torphy50@yahoo.com", "candice7@gmail.com", "brown.harber24@hotmail.com", "thelma.smith66@gmail.com"];
+
+// Ask the user to enter their email
+
+
+
+
+
+
+
+
+// Print an appropriate message about the check result
+
+
+
+
+
+
